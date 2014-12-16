503c73cac6ca7b59fc585b3a9d237ddd9c844f8e
Evan O'Keeffe <evanokeeffe@gmial.com>
2014-12-16 20:06:46 +0000

    need to reimplement futureGPSPosition with vincenty direct 3d formula. Current function is only giving current position. Doesn't look to bad

--- scripts/predict_future_position.py
+++ scripts/predict_future_position.py
@@ -1,49 +1,23 @@
 from math import *
 
-radiusEarthKm = 6371
-radiusEarthM  =  radiusEarthKm*1000
-d = 15
+R = 6371
+d = 15/1000
 
 angleRadHeading = radians(185.4114)
-lat0 = 53.3077
-lng0 = -6.2330
-Speed = 15
-time_interval = 5
 
-kmDistance = Speed * (time_interval/1000) / 3600
+lat = 53.3077
+lon = -6.2330
 
-distRatio = kmDistance/radiusEarthM
-distRatioSine = sin(distRatio)
-distRatioCosine = cos(distRatio)
+lat1 = radians(lat) #Current lat point converted to radians
+lon1 = radians(lon) #Current long point converted to radians
 
-startLatRad = lat0 #radians(lat0)
-startLongRad = lng0#radians(lng0)
+lat2 = asin(sin(lat1)*cos(d/R) + cos(lat1)*sin(d/R)*cos(angleRadHeading))
+lon2 = lon1 + atan2(cos(d/R)-sin(lat1)*sin(lat2), sin(angleRadHeading)*sin(d/R)*cos(lat1))
 
-startLatCos = cos(startLatRad)
-startLatSin = sin(startLongRad)
+print "%.6f" % lat2,"%.6f" % lon2
 
-endLatRad = asin(
-    (startLatSin*distRatioCosine) +
-    (startLatCos*distRatioSine * cos(angleRadHeading))
-)
+lat2 = degrees(lat2)
+lon2 = degrees(lon2)
 
-endLongRad = startLongRad + atan2(sin(angleRadHeading) *
-    distRatioSine * startLatCos, 
-    distRatioCosine - startLatSin * sin(endLatRad)
-)
-
-newLat = degrees(endLatRad)
-newLong = degrees(endLongRad)
-print "Radians"
-print "Future Lat:",endLatRad
-print "Future Lon:",endLongRad
-print "Degrees"
-print "Future Lat:",newLat
-print "Future Lon:",newLong
-
-
-lat2 = degrees((d/radiusEarthM) * cos(angleRadHeading)) + lat0
-long2 = degrees((d/(radiusEarthM*sin(radians(lat2)))) * sin(angleRadHeading)) + lng0
-
-print "Second Implementation"
-print "Lat:",lat2,",Lng:",long2
+print "Projection"
+print "%.6f" % lat2,"%.6f" % lon2
